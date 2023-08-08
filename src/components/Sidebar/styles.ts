6501902eafb422f236cc5b6cc74e96bba8dcac3c
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-08 15:57:26 -0300

    Feat: styled Sidebar component

---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,45 @@
+import { styled } from 'styled-components'
+
+const animationDuration = 300
+
+interface SidebarContainerProps {
+  variant?: string
+}
+
+export const SidebarContainer = styled.header<SidebarContainerProps>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  padding: 2rem 1rem;
+
+  background: ${(props) => props.theme['gray-900']};
+  transition: width ${animationDuration}ms ease-in-out;
+  width: ${(props) => (props.variant === 'true' ? '20rem' : '6rem')};
+`
+
+export const DropdownMenuButton = styled.button`
+  width: 40px;
+  height: 40px;
+  border: none;
+  cursor: pointer;
+  align-items: center;
+  display: inline-flex;
+  justify-content: center;
+  background: transparent;
+
+  svg {
+    color: ${(props) => props.theme['gray-300']};
+  }
+`
+
+export const NavBarContainer = styled.nav<SidebarContainerProps>`
+  min-width: 4rem;
+  max-width: 1000px;
+  margin-top: 5rem;
+
+  gap: 2rem;
+  display: flex;
+  flex-direction: column;
+`
