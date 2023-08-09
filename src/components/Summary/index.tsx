d1a583f25a990d102705d72524dfbe6e93c3c8cf
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-09 15:50:48 -0300

    Feat: created Summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,42 @@
+import * as S from './styles'
+import { useSummary } from '../../hooks/useSummary'
+import { priceFormatter } from '../../utils/formatter'
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+
+export function Summary() {
+  const summary = useSummary()
+
+  return (
+    <S.SummaryContainer>
+      <S.SummaryCard>
+        <header>
+          <span>Entradas</span>
+
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+
+        <strong>{priceFormatter.format(summary.income)}</strong>
+      </S.SummaryCard>
+
+      <S.SummaryCard>
+        <header>
+          <span>Saídas</span>
+
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+
+        <strong>{priceFormatter.format(summary.expense)}</strong>
+      </S.SummaryCard>
+
+      <S.SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+
+        <strong>{priceFormatter.format(summary.total)}</strong>
+      </S.SummaryCard>
+    </S.SummaryContainer>
+  )
+}
