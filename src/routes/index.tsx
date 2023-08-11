7cbff98fc53eb5464ba31351096dc8ccb9a2ea95
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-11 15:34:46 -0300

    Feat: little changes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
 import { BrowserRouter } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
+import { useAuth } from '../contexts/auth/AuthProvider'
 
 export function Routes() {
   const { user } = useAuth()
