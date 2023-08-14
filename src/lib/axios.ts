7ae3adff9e1439d1354bd1f1b475dd18d9419c9b
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-14 14:30:58 -0300

    Feat: created apiPrivate from axios and added the interceptor on responses api

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,28 @@
 import axios from 'axios'
+import { useRefreshAccessToken } from '../hooks/useRefreshAccessToken'
 
 export const api = axios.create({
   baseURL: 'http://localhost:3000',
 })
+
+export const apiPrivate = axios.create({
+  baseURL: 'http://localhost:3000',
+  headers: { 'Content-Type': 'application/json' },
+})
+
+apiPrivate.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const prevRequest = error?.config
+
+    if (error?.response?.status === 401 && !prevRequest?.sent) {
+      prevRequest.sent = true
+      const newAccessToken = await useRefreshAccessToken()
+      prevRequest.headers.Authorization = `Bearer ${newAccessToken}`
+      return apiPrivate(prevRequest)
+    }
+
+    return Promise.reject(error)
+  },
+)
