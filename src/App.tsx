ebc83dfd5ccf65f0ff25a3b41f84ca4be6c1706e
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-15 15:45:20 -0300

    Feat: added the compose method and remove providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
 import { Routes } from './routes'
+import Compose from './contexts/Compose'
 import { GlobalStyle } from './styles/global'
 import 'react-toastify/dist/ReactToastify.css'
 import { ToastContainer } from 'react-toastify'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { AuthProvider } from './contexts/auth/AuthProvider'
-import { TransactionsProvider } from './contexts/TransactionsContext'
+import { IncomeProvider } from './contexts/income/IncomeProvider'
+import { ExpenseProvider } from './contexts/expense/ExpenseProvider'
+import { TransactionsProvider } from './contexts/transactions/TransactionsProvider'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <AuthProvider>
-        <TransactionsProvider>
-          <Routes />
-          <ToastContainer
-            closeOnClick
-            theme="light"
-            autoClose={3000}
-            toastClassName="foo"
-            limit={3}
-          />
-        </TransactionsProvider>
-      </AuthProvider>
+
+      <Compose components={[AuthProvider, TransactionsProvider]}>
+        <Routes />
+        <ToastContainer
+          closeOnClick
+          theme="light"
+          autoClose={3000}
+          toastClassName="foo"
+          limit={3}
+        />
+      </Compose>
     </ThemeProvider>
   )
 }
