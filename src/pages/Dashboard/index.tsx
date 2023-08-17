5edbd3ec83cf653bfc9c4d67f157b527e2028f4f
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-17 15:52:50 -0300

    Feat: created cardsToSummary and passed in props to cards summary

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,13 +4,35 @@ import { Summary } from '../../components/Summary'
 import { apiPrivate } from '../../lib/axios'
 import { toast } from 'react-toastify'
 import { MonthlyBalance } from './components/MonthlyBalance'
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { useSummary } from '../../hooks/useSummary'
 
 export function Dashboard() {
+  const summary = useSummary()
+
+  const cardsToSummary = [
+    {
+      title: 'Entradas',
+      icon: <ArrowCircleUp size={32} color="#00b37e" />,
+      value: summary.income,
+    },
+    {
+      title: 'Saídas',
+      icon: <ArrowCircleDown size={32} color="#f75a68" />,
+      value: summary.expense,
+    },
+    {
+      title: 'Total',
+      icon: <CurrencyDollar size={32} color="#fff" />,
+      value: summary.total,
+    },
+  ]
+
   return (
     <S.DashboardContainer>
       <S.DashboardTitle>Dashboard</S.DashboardTitle>
 
-      <Summary />
+      <Summary cards={cardsToSummary} />
 
       <S.DashboardSubTitle>Balanço mensal</S.DashboardSubTitle>
 
