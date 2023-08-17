0ed27127bb2012c3ae2aec7b8b4acb601dc52f4c
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-17 15:54:12 -0300

    Feat: cadded providers to especific routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,14 +2,37 @@ import { Dashboard } from '../pages/Dashboard'
 import { Route, Routes } from 'react-router-dom'
 import { Transactions } from '../pages/Transactions'
 import { DefaultLayout } from '../layouts/DefaultLayout'
+import { TransactionsProvider } from '../contexts/transactions/TransactionsProvider'
+import { Incomes } from '../pages/Incomes'
+import { Expenses } from '../pages/Expenses'
+import { IncomeProvider } from '../contexts/income/IncomeProvider'
+import { ExpenseProvider } from '../contexts/expense/ExpenseProvider'
 
 export function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/transactions" element={<Transactions />} />
-      </Route>
-    </Routes>
+    <TransactionsProvider>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/transactions" element={<Transactions />} />
+          <Route
+            path="/transactions/receitas"
+            element={
+              <IncomeProvider>
+                <Incomes />
+              </IncomeProvider>
+            }
+          />
+          <Route
+            path="/transactions/despesas"
+            element={
+              <ExpenseProvider>
+                <Expenses />
+              </ExpenseProvider>
+            }
+          />
+        </Route>
+      </Routes>
+    </TransactionsProvider>
   )
 }
