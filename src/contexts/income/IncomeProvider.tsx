73dfc6437219449bbdb60c93ed2bda9147651c0f
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-21 15:38:25 -0300

    Refactor: added the show toast error and trycatch block

--- src/contexts/income/IncomeProvider.tsx
+++ src/contexts/income/IncomeProvider.tsx
@@ -1,24 +1,39 @@
-import { ReactNode, useCallback, useEffect, useState } from 'react'
-import { IncomeContext } from './IncomeContext'
-import { apiPrivate } from '../../lib/axios'
+import { toast } from 'react-toastify'
 import { Income } from '../../@types/mockes'
+import { apiPrivate } from '../../lib/axios'
+import { IncomeContext } from './IncomeContext'
+import { ReactNode, useCallback, useEffect, useState } from 'react'
 
 interface IncomeProviderProps {
   children: ReactNode
+}
+
+function showToastError(message: string) {
+  toast.error(message)
 }
 
 export function IncomeProvider({ children }: IncomeProviderProps) {
   const [incomes, setIncomes] = useState<Income[]>([])
 
   const fetchIncomes = useCallback(async (month?: string, year?: string) => {
-    const response = await apiPrivate.get('incomes/get-all', {
-      params: {
-        month,
-        year,
-      },
-    })
+    try {
+      const response = await apiPrivate.get('incomes/get-all', {
+        params: {
+          month,
+          year,
+        },
+      })
 
-    setIncomes(response.data)
+      setIncomes(response.data)
+    } catch (error: any) {
+      if (error.response.status) {
+        showToastError(error.response.data.message)
+      } else {
+        showToastError(
+          'Não foi possível acessar sua conta, tente novamente mais tarde!',
+        )
+      }
+    }
   }, [])
 
   useEffect(() => {
