d6e9b255174e114092588f06c693c1793c876f14
Grégory Alvim <gregori.jagua@gmail.com>
2023-08-29 09:26:18 -0300

    Feat: added method to create a new expense

--- src/contexts/expense/ExpenseContext.tsx
+++ src/contexts/expense/ExpenseContext.tsx
@@ -4,6 +4,7 @@ import { createContext } from 'use-context-selector'
 interface ExpenseContextType {
   expenses: Expense[]
   fetchExpenses: (month: string, year: string) => void
+  createNewExpense: (data: Expense) => void
 }
 
 export const ExpenseContext = createContext({} as ExpenseContextType)
--- src/contexts/expense/ExpenseProvider.tsx
+++ src/contexts/expense/ExpenseProvider.tsx
@@ -2,7 +2,9 @@ import { toast } from 'react-toastify'
 import { apiPrivate } from '../../lib/axios'
 import { Expense } from '../../@types/mockes'
 import { ExpenseContext } from './ExpenseContext'
-import { ReactNode, useCallback, useEffect, useState } from 'react'
+import { ReactNode, useCallback, useEffect, useReducer, useState } from 'react'
+import { expensesReducer } from '../../reducers/expenses/reducer'
+import { fetchExpensesAction } from '../../reducers/expenses/actions'
 
 interface ExpenseProviderProps {
   children: ReactNode
@@ -13,7 +15,11 @@ function showToastError(message: string) {
 }
 
 export function ExpenseProvider({ children }: ExpenseProviderProps) {
-  const [expenses, setExpenses] = useState<Expense[]>([])
+  const [expensesState, dispatch] = useReducer(expensesReducer, {
+    expenses: [],
+  })
+
+  const { expenses } = expensesState
 
   const fetchExpenses = useCallback(async (month?: string, year?: string) => {
     try {
@@ -24,24 +30,38 @@ export function ExpenseProvider({ children }: ExpenseProviderProps) {
         },
       })
 
-      setExpenses(response.data)
+      dispatch(fetchExpensesAction(response.data))
     } catch (error: any) {
       if (error.response.status) {
         showToastError(error.response.data.message)
       } else {
-        showToastError(
-          'Não foi possível acessar sua conta, tente novamente mais tarde!',
-        )
+        showToastError('Algo deu errado, tente novamente!')
       }
     }
   }, [])
+
+  async function createNewExpense(data: Expense) {
+    const { value, description, category, date, isPay } = data
+
+    await apiPrivate.post('expenses/create-expense', {
+      category_uuid: category.id,
+      description,
+      isPay,
+      value,
+      date,
+    })
+
+    fetchExpenses()
+  }
 
   useEffect(() => {
     fetchExpenses()
   }, [fetchExpenses])
 
   return (
-    <ExpenseContext.Provider value={{ expenses, fetchExpenses }}>
+    <ExpenseContext.Provider
+      value={{ expenses, fetchExpenses, createNewExpense }}
+    >
       {children}
     </ExpenseContext.Provider>
   )
