a740fce0931823efcc8f89b4c7f06a82ad7c9263
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-02 15:05:09 -0300

    Feat: added new function to formated date input value

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,9 +1,20 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format, parse } from 'date-fns'
 
 export const dateStringFormatter = (date: string | undefined) => {
   if (date !== undefined) {
     const parsedDate = parseISO(date)
     const formattedDate = format(parsedDate, 'dd/MM/yyyy')
+
+    return formattedDate
+  }
+
+  return 'Data incorreta'
+}
+
+export const dateInputFormatter = (date: string | undefined) => {
+  if (date !== undefined) {
+    const inputDate = parse(date, 'yyyy-MM-dd', new Date())
+    const formattedDate = format(inputDate, 'dd/MM/yyyy')
 
     return formattedDate
   }
