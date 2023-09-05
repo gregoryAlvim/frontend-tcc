2abb31003014908aff858ebb906df5051364b102
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-05 15:50:31 -0300

    Refactor: removed from app componentes and added to transactions components

---
+++ src/pages/Transactions/components/UpdateDialogButton/index.tsx
@@ -0,0 +1,26 @@
+import * as S from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+import { UpdateTransactionModal } from '../UpdateTransactionModal'
+import { Income, Expense } from '../../../../@types/mockes'
+
+interface DialogButtonProps {
+  icon: React.ReactNode
+  type?: 'income' | 'expense'
+  data: Income | Expense
+}
+
+export function UpdateDialogButton({
+  icon,
+  data,
+  type = 'income',
+}: DialogButtonProps) {
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <S.NewTransactionButton variant={type}>{icon}</S.NewTransactionButton>
+      </Dialog.Trigger>
+
+      <UpdateTransactionModal data={data} typeOfButton={type} />
+    </Dialog.Root>
+  )
+}
---
+++ src/pages/Transactions/components/UpdateDialogButton/styles.ts
@@ -0,0 +1,17 @@
+import { styled } from 'styled-components'
+
+interface NewTransactionButtonProps {
+  variant?: 'income' | 'expense'
+}
+
+export const NewTransactionButton = styled.button<NewTransactionButtonProps>`
+  background-color: transparent;
+  border: none;
+  color: ${(props) => props.theme['purple-300']};
+  cursor: pointer;
+
+  &:hover {
+    color: ${(props) => props.theme['purple-500']};
+    transition: all 0.3s;
+  }
+`
