b2750529193d322de9f5c44d65191fc88ffb4142
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-06 16:42:46 -0300

    Refactor: little changes

--- src/components/ModalScreen/styles.ts
+++ src/components/ModalScreen/styles.ts
@@ -5,12 +5,15 @@ export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
   width: 100vw;
   height: 100vh;
+
   inset: 0;
   background: rgba(0, 0, 0, 0.75);
 `
 
 export const Content = styled(Dialog.Content)`
   min-width: 32rem;
+  max-height: 60rem;
+  overflow-y: auto;
   border-radius: 6px;
   padding: 2.5rem 3rem;
   background: ${(props) => props.theme['gray-800']};
