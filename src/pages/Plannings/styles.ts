df09e2e2d17e15608fb443090b5fde1f7fdf5939
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-06 16:39:14 -0300

    Feat: created Plannings page

---
+++ src/pages/Plannings/styles.ts
@@ -0,0 +1,21 @@
+import { styled } from 'styled-components'
+
+export const PlanningsContainer = styled.main`
+  width: 100%;
+  max-width: 1120px;
+  margin: 8rem auto 0;
+  padding: 0 1.5rem;
+`
+
+export const PlanningsHeader = styled.header`
+  width: 100%;
+  height: 4rem;
+  display: flex;
+  justify-content: space-between;
+
+  margin-bottom: 2rem;
+`
+
+export const PlanningsTitle = styled.h1`
+  margin-bottom: 2rem;
+`
