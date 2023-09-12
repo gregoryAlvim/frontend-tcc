901966f189c47be3537385322c4129f9776183a0
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-12 16:51:28 -0300

    Feat: added react hook form to handle data from inputs

--- src/pages/Plannings/components/NewPlanningStepOne/index.tsx
+++ src/pages/Plannings/components/NewPlanningStepOne/index.tsx
@@ -1,22 +1,52 @@
 import * as z from 'zod'
 import * as S from './styles'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-export function NewPlanningStepOne() {
+interface NewPlanningStepOneProps {
+  handleSetDataStepOne: (totalMonthlyRevenue: number) => void
+}
+
+export function NewPlanningStepOne({
+  handleSetDataStepOne,
+}: NewPlanningStepOneProps) {
   const newPlanningStepOneSchema = z.object({
     totalMonthlyRevenue: z.number(),
   })
 
+  type NewPlanningStepOneInputs = z.infer<typeof newPlanningStepOneSchema>
+
+  const {
+    reset,
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<NewPlanningStepOneInputs>({
+    resolver: zodResolver(newPlanningStepOneSchema),
+  })
+
+  async function handleTotalMonthlyRevenue(data: NewPlanningStepOneInputs) {
+    const { totalMonthlyRevenue } = data
+    handleSetDataStepOne(totalMonthlyRevenue)
+  }
+
   return (
-    <S.FormStepOne>
-      <p>Primeiro passo</p>
-      <span>Informe qual é sua receita mensal total.</span>
+    <S.FormStepOne onSubmit={handleSubmit(handleTotalMonthlyRevenue)}>
+      <strong>Primeiro passo</strong>
+      <span>Informe qual é seu orçamento mensal de gastos.</span>
 
       <S.InputContainer>
         <label htmlFor="totalMonthlyRevenue"> Insira um valor</label>
 
-        <input id="totalMonthlyRevenue" type="number" />
+        <input
+          type="number"
+          id="totalMonthlyRevenue"
+          {...register('totalMonthlyRevenue', { valueAsNumber: true })}
+        />
 
-        <button type="submit">Continuar</button>
+        <button type="submit" disabled={isSubmitting}>
+          Continuar
+        </button>
       </S.InputContainer>
     </S.FormStepOne>
   )
