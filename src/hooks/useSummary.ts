3d9c6eed3530fe0a8f39bf1d2bbd2ef28e019858
Grégory Alvim <gregori.jagua@gmail.com>
2023-09-22 09:10:45 -0300

    Feat: created method fetch transactions on summary hook and added on dashboard

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,7 +1,23 @@
-import { useExpensesSummary } from './useExpenseSummary'
 import { useIncomesSummary } from './useIncomesSummary'
+import { useExpensesSummary } from './useExpenseSummary'
+import { useContextSelector } from 'use-context-selector'
+import { IncomeContext } from '../contexts/income/IncomeContext'
+import { ExpenseContext } from '../contexts/expense/ExpenseContext'
 
 export function useSummary() {
+  const fetchExpenses = useContextSelector(ExpenseContext, (context) => {
+    return context.fetchExpenses
+  })
+
+  const fetchIncomes = useContextSelector(IncomeContext, (context) => {
+    return context.fetchIncomes
+  })
+
+  function fetchTransactions(month: string, year: string) {
+    fetchExpenses(month, year)
+    fetchIncomes(month, year)
+  }
+
   const summaryIncome = useIncomesSummary()
   const summaryExpense = useExpensesSummary()
 
@@ -11,5 +27,8 @@ export function useSummary() {
     total: summaryIncome.previsto - summaryExpense.previsto,
   }
 
-  return summary
+  return {
+    summary,
+    fetchTransactions,
+  }
 }
